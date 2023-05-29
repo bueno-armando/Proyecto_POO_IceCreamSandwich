0a8f71424f7aa6cbb104c2b65de0b13a35bb6353
Levi Garcia <58457288+levigg4@users.noreply.github.com>
2023-05-29 10:28:50 -0600

    Version 2.1

--- Proyecto_POO_IceCreamSandwich-main/PROYECTO_OOP_BASQUETBOL/src/proyecto_oop_basquetbol/PROYECTO_OOP_BASQUETBOL.java
+++ Proyecto_POO_IceCreamSandwich-main/PROYECTO_OOP_BASQUETBOL/src/proyecto_oop_basquetbol/PROYECTO_OOP_BASQUETBOL.java
@@ -1,5 +1,6 @@
 package proyecto_oop_basquetbol;
 import Entities.*;
+import SubClases.Player;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -8,12 +9,12 @@ import java.util.Scanner;
  * @author IceCreamSandwich
  */
 public class PROYECTO_OOP_BASQUETBOL implements Utilities{
-
     public static void main(String[] args) {
         System.out.println(mainMenu());
         
     }
     public static int mainMenu(){
+        int indiceEquipo=0;
         do{
             try{
                 int x;
@@ -23,10 +24,10 @@ public class PROYECTO_OOP_BASQUETBOL implements Utilities{
                 x = sc.nextInt();
                 switch(x){
                     case 1:
-                        System.out.println("Elegiste la 1");
+                        menuAddTeam(indiceEquipo);
                         return x;
                     case 2:
-                        System.out.println("Elegiste la 2");
+                        System.out.println("VE LA ESTADISTICA");
                         return x;
                     default:
                         System.out.println("Opcion Inexistente");
@@ -38,6 +39,46 @@ public class PROYECTO_OOP_BASQUETBOL implements Utilities{
             }
         }while(true);         
         
+    }
+    public static int menuAddTeam(int indiceGlobal){
+        Team[] numEquipos = new Team[100];
+        System.out.println("*****EQUIPO 1*****");
+        Scanner sc = new Scanner(System.in);
+        do{
+            try{
+            int opcSelectTeam;
+            System.out.println("Selecciona tu equipo"
+                          +"(1)Existente"
+                          +"(2)Crear Uno");
+            opcSelectTeam = sc.nextInt(); 
+                switch(opcSelectTeam){
+                    case 1:
+                        //abrir o desplegar todos los equipos existentes (FILES CHOOSER) ABRIR ARCHIVOS
+                        return opcSelectTeam;
+                    case 2:
+                        System.out.println("Agregar equipo 1");
+                        addTeam(indiceGlobal, numEquipos);
+                        indiceGlobal++;
+                        return opcSelectTeam;
+                    default:
+                        System.out.println("Opcion Inexistente");
+                        break;
+                }
+            }
+            catch(InputMismatchException e){
+                System.out.println("Input no valido, intenta de nuevo");
+            }
+        }while(true);
+        
+    }
+    public static void addTeam(int indice, Team[] objEquipo){
+        Team CreatedTeam = objEquipo[indice];
+        Scanner sc = new Scanner(System.in);
+        String nombreEquipo = sc.nextLine();
+        CreatedTeam.setTeamName(nombreEquipo);
+        System.out.println("Cuantos jugadores agregaras? ");
+        Player[] players;
+       
     }
 
     @Override
